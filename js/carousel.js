f5c2fd588c5e1375edcb7a4863204ad976b825b6
nath-sans <cristinenathaly5@gmail.com>
2025-08-04 10:58:30 -0300

    adicionando no css

--- js/carousel.js
+++ js/carousel.js
@@ -1,31 +1,28 @@
-const images = [
-    'img/espaco1.jpg',
-    'img/espaco2.jpg',
-    'img/espaco3.jpg'
-];
+let slideIndex = 1;
+showSlides(slideIndex);
 
-let currentIndex = 0;
-
-const imgElement = document.querySelector('.carousel-image');
-const prevButton = document.querySelector('.prev');
-const nextButton = document.querySelector('.next');
-
-function updateImage() {
-    imgElement.src = images[currentIndex];
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
 
-function showNextImage() {
-    currentIndex = (currentIndex + 1) % images.length;
-    updateImage();
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
 }
 
-function showPrevImage() {
-    currentIndex = (currentIndex - 1 + images.length) % images.length;
-    updateImage();
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
 }
-
-nextButton.addEventListener('click', showNextImage);
-prevButton.addEventListener('click', showPrevImage);
-
-// Initialize the carousel with the first image
-updateImage();
