4548326d53b04acb67f8eb225847d1b2da6a50e0
kezia_slv <keziaoficialk@gmail.com>
2025-09-18 11:53:56 -0300

    feat: crud sebo

--- backend/Controllers/TesteModels.php
+++ backend/Controllers/TesteModels.php
@@ -5,5 +5,8 @@ require_once __DIR__ . '/../Database/Database.php';
 
 $usuario = new Usuario($db);
 
-$resultado = $usuario->buscarUsuarios();
+//$resultado = $usuario->buscarUsuarios();
+//$resultado = $usuario->inserirUsuarios("Kezia", "kezia@gmail.com", "senha123", "cliente");
+//$resultado = $usuario->atualizarUsuarios("23", "Junior Ferreira", "juniorf@gmail.com", "senha123", "admin");
+$resultado = $usuario->excluirUsuario(1);
 var_dump($resultado);
--- backend/Models/Usuario.php
+++ backend/Models/Usuario.php
@@ -17,9 +17,67 @@ class Usuario{
     
     // Buscar todos os usuários não excluídos
     function buscarUsuarios(){
-        $sql = "SELECT * FROM usuarios WHERE excluido_em IS NULL";
+        $sql = "SELECT * FROM tbl_usuario WHERE excluido_em IS NULL";
         $stmt = $this->db->prepare($sql);
         $stmt->execute();
-        return $stmt->fetvchAll(PDO::FETCH_ASSOC);
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
+    
+
+    // Inserir Usuários
+
+    function inserirUsuarios($nome_usuario, $email_usuario, $senha_usuario, $tipo_usuario){
+        $sql = 'INSERT INTO tbl_usuario (nome_usuario, email_usuario, 
+        senha_usuario, tipo_usuario) 
+        VALUES (:nome, :email, :senha, :tipo)';
+        $stmt = $this->db->prepare($sql);
+        $stmt->bindParam(':nome', $nome_usuario);
+        $stmt->bindParam(':email', $email_usuario);
+        $stmt->bindParam(':senha', $senha_usuario);
+        $stmt->bindParam(':tipo', $tipo_usuario);
+        if($stmt->execute()){
+            return $this->db->lastInsertId();
+        }else{
+            return false;
+        }
     }
+
+    // Atualizar Usuários
+
+    function atualizarUsuarios($id_usuario, $nome_usuario, $email_usuario, $senha_usuario, $tipo_usuario){
+        $senha_usuario = password_hash($senha_usuario, PASSWORD_DEFAULT);
+        $sql = 'UPDATE tbl_usuario 
+        SET nome_usuario = :nome,
+         email_usuario = :email,
+         senha_usuario = :senha,
+         tipo_usuario = :tipo
+        WHERE id_usuario = :id';
+        $stmt = $this->db->prepare($sql);
+        $stmt->bindParam(':id', $id_usuario);
+        $stmt->bindParam(':nome', $nome_usuario);
+        $stmt->bindParam(':email', $email_usuario);
+        $stmt->bindParam(':senha', $senha_usuario);
+        $stmt->bindParam(':tipo', $tipo_usuario);
+        if($stmt->execute()){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    function excluirUsuario($id_usuario){
+        $dataatual = date('Y-m-d H:i:s');
+        $sql = "UPDATE tbl_usuario SET 
+        excluido_em = :atual
+        WHERE id_usuario = :id";
+        $stmt = $this->db->prepare($sql);
+        $stmt = bindParam(':id', $id_usuario);
+        $stmt = bindParam(':atual', $dataatual);
+        if($stmt->execute()){
+            return true;
+        }else{
+            return false;
+        }
+}
+
+}
