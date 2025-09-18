0c068fe914248d6158364da76f4ddb64e1473cf2
kezia_slv <keziaoficialk@gmail.com>
2025-09-18 09:14:58 -0300

    feat: nathaly e kezia

--- backend/Controllers/TesteModels.php
+++ backend/Controllers/TesteModels.php
@@ -6,5 +6,4 @@ require_once __DIR__ . '/../Database/Database.php';
 $usuario = new Usuario($db);
 
 $resultado = $usuario->buscarUsuarios();
-$resultado = $usuario->excluirUsuario();
 var_dump($resultado);
