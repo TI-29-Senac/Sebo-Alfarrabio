be9ce0ac92f7be22f306b73997298d310f5780ca
KEZIA SILVA DO ESPIRITO SANTO <kezia.sesanto@senacsp.edu.br>
2025-10-16 08:17:45 -0300

    Teste 3 (ainda não funcionou completamente)

--- backend/Controllers/UsuarioController.php
+++ backend/Controllers/UsuarioController.php
@@ -1,89 +1,51 @@
 <?php
 namespace Sebo\Alfarrabio\Controllers;
-
+ 
 use Sebo\Alfarrabio\Models\Usuario;
-use Sebo\Alfarrabio\Database\Database;
-use Sebo\Alfarrabio\Core\View;
-use Sebo\Alfarrabio\Core\Redirect;
-use Sebo\Alfarrabio\Validadores\UsuarioValidador;
-use Sebo\Alfarrabio\Core\FileManager;
+use Sebo\Alfarrabio\Database\Database; use Sebo\Alfarrabio\Core\View;
 
 
 class UsuarioController {
     public $usuario;
-    public $db;
-    public $gerenciarImagem;
-    public function __construct() {
-        $this->db = Database::getInstance();
-        $this->usuario = new Usuario($this->db);
-        $this->gerenciarImagem = new FileManager('upload');
+    public $db; public function __construct() {
+        $this->db = Database::getInstance(); $this->usuario = new Usuario($this->db);
     }
-
-    public function salvarUsuario(){
-        $erros = UsuarioValidador::ValidarEntradas($_POST);
-        if(!empty($erros)){
-        
-             Redirect::redirecionarComMensagem("/usuario/criar","error", implode("<br>", $erros));
-        }
-        $imagem= $this->gerenciarImagem->salvarArquivo($_FILES['imagem'], 'usuario');
-         if($this->usuario->inserirUsuario(
-             $_POST["nome_usuario"],
-             $_POST["email_usuario"],
-             $_POST["senha_usuario"],
-             $_POST["tipo_usuario"],
-             "Ativo",
-             $imagem
-         )){
-             Redirect::redirecionarComMensagem("usuario/listar", "success", "Usuário cadastrado com sucesso!");
-         }else{
-             Redirect::redirecionarComMensagem("usuario/criar", "error", "Erro ao cadastrar usuário");
-         }
-     }
     // index
     public function index() {
         $resultado = $this->usuario->buscarUsuarios();
+        return $resultado;
         var_dump($resultado);
     }
-    public function viewListarUsuarios(){
-        $dados = $this->usuario->buscarUsuarios(); 
-        
-        View::render("usuario/index", 
-        [
-            "usuarios"=> $dados
-        ]
-    );
+ 
+    public function viewListarUsuarios() {
+        $dados = $this->usuario->buscarUsuarios();
+        View::render("usuario/index", ["usuarios" => $dados]);
     }
-
-    public function viewCriarUsuarios(){
-        View::render("usuario/create", []);
+ 
+    public function viewCriarUsuarios() {
+       View::render("usuario/create", []);
     }
-
-    public function viewEditarUsuarios($id){
-        $dados = $this->usuario->atualizarUsuario($id);
-        foreach($dados as $usuario){
-            $dados = $usuario;
-        }
-        View::render("usuario/edit", ["usuario" => $dados]);
+ 
+    public function viewEditarUsuarios() {
+        View::render("usuario/edit", []);
     }
-
-    public function viewExcluirUsuarios($id){
-        View::render("usuario/delete", ["id_usuario" => $id_usuario]);
+ 
+    public function viewExcluirUsuarios() {
+        View::render("usuario/delete", []);
     }
-
-    public function relatorioUsuario($id_usuario, $data1, $data2){
-        View::render("usuario/relatorio",
-        ["id"=>$id_usuario, "data1"=> $data1, "data2"=> $data2]
-    );
+ 
+    public function salvarUsuario() {
+        var_dump($_POST);
+        echo "Salvar usuario";
     }
-
-    
-
-    public function atualizarUsuario(){
-        echo "Atualizar Usuario";
+ 
+    public function atualizarUsuario() {
+        echo "Atualizar usuario";
     }
-
-    public function deletarUsuario(){
-        echo "Deletar Usuario";
+ 
+    public function deletarUsuario() {
+        echo "Deletar usuario";
     }
-
+ 
+ 
 }
--- backend/Models/Usuario.php
+++ backend/Models/Usuario.php
@@ -2,85 +2,64 @@
 namespace Sebo\Alfarrabio\Models;
 use PDO;
 class Usuario{
-    private $id_usuario;
-    private $nome_usuario;
-    private $email_usuario;
+    public $id_usuario;
+    public $nome_usuario;
+    public $email_usuario;
     private $senha_usuario;
-    private $tipo_usuario;
+    public $tipo_usuario;
     private $criado_em;
     private $atualizado_em;
     private $excluido_em;
     private $db;
-    // contrutor inicializa a classe e ou atributos
-    public function __construct($db){
-       $this->db = $db;
+ 
+ 
+        public function __construct($db){
+        $this->db = $db;
     }
-    // metodo de buscar todos os usuarios read
+   
+    // Buscar todos os usuários não excluídos
     function buscarUsuarios(){
-        $sql = "SELECT * FROM tbl_usuario";
+        $sql = "SELECT * FROM tbl_usuario WHERE excluido_em IS NULL";
         $stmt = $this->db->prepare($sql);
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
-
-    // metodo de buscar todos usuario por email read
-    function buscarUsuariosPorEMail($email_usuario){
-        $sql = "SELECT * FROM tbl_usuario where email_usuario = :email and excluido_em IS NULL";
-        $stmt = $this->db->prepare($sql);
-        $stmt->bindParam(':email', $email_usuario); 
-        $stmt->execute();
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
-    }
-
-    function buscarUsuariosPorID($id_usuario){
-        $sql = "SELECT * FROM tbl_usuario where id_usuario = :id_usuario";
-        $stmt = $this->db->prepare($sql);
-        $stmt->bindParam(':id_usuario', $id); 
-        $stmt->execute();
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
-    }
-
-    // metodo de inserir usuario create
-    function inserirUsuario($nome_usuario,
-     $email_usuario, 
-     $senha_usuario, 
-     $tipo_usuario
-     ){
-        $senha_usuario = password_hash($senha_usuario, PASSWORD_DEFAULT);
-        $sql = "INSERT INTO tbl_usuario (nome_usuario, email_usuario, 
-        senha_usuario, tipo_usuario) 
-                VALUES (:nome, :email, :senha, :tipo)";
+   
+ 
+    // Inserir Usuários
+ 
+    function inserirUsuarios($nome_usuario, $email_usuario, $senha_usuario, $tipo_usuario){
+        $sql = 'INSERT INTO tbl_usuario (nome_usuario, email_usuario,
+        senha_usuario, tipo_usuario)
+        VALUES (:nome, :email, :senha, :tipo)';
         $stmt = $this->db->prepare($sql);
         $stmt->bindParam(':nome', $nome_usuario);
         $stmt->bindParam(':email', $email_usuario);
         $stmt->bindParam(':senha', $senha_usuario);
         $stmt->bindParam(':tipo', $tipo_usuario);
-
         if($stmt->execute()){
             return $this->db->lastInsertId();
         }else{
             return false;
         }
     }
-
-    // metodo de atualizar o usuario update
-    function atualizarUsuario($id_usuario, $nome_usuario, $email_usuario, $senha_usuario, $tipo_usuario){
+ 
+    // Atualizar Usuários
+ 
+    function atualizarUsuarios($id_usuario, $nome_usuario, $email_usuario, $senha_usuario, $tipo_usuario){
         $senha_usuario = password_hash($senha_usuario, PASSWORD_DEFAULT);
-        $dataatual = date('Y-m-d H:i:s');
-        $sql = "UPDATE tbl_usuario SET nome_usuario = :nome,
-         email_usuario = :email, 
-         senha_usuario = :senha, 
-         tipo_usuario = :tipo,
-         atualizado_em = :atual,
-         WHERE id_usuario = :id";
+        $sql = 'UPDATE tbl_usuario
+        SET nome_usuario = :nome,
+         email_usuario = :email,
+         senha_usuario = :senha,
+         tipo_usuario = :tipo
+        WHERE id_usuario = :id';
         $stmt = $this->db->prepare($sql);
         $stmt->bindParam(':id', $id_usuario);
         $stmt->bindParam(':nome', $nome_usuario);
         $stmt->bindParam(':email', $email_usuario);
         $stmt->bindParam(':senha', $senha_usuario);
         $stmt->bindParam(':tipo', $tipo_usuario);
-
-        $stmt->bindParam(':atual', $dataatual);
         if($stmt->execute()){
             return true;
         }else{
@@ -88,13 +67,13 @@ class Usuario{
         }
     }
     // metodo de inativar o usuario delete
-    function excluirUsuario($id_usuario){
+    function excluirUsuario($id){
         $dataatual = date('Y-m-d H:i:s');
         $sql = "UPDATE tbl_usuario SET
          excluido_em = :atual
          WHERE id_usuario = :id";
         $stmt = $this->db->prepare($sql);
-        $stmt->bindParam(':id', $id_usuario);
+        $stmt->bindParam(':id', $id);
         $stmt->bindParam(':atual', $dataatual);
         if($stmt->execute()){
             return true;
@@ -103,13 +82,13 @@ class Usuario{
         }
     }
 // metodo de ativar o usuario excluido
-    function ativarUsuario($id_usuario){
+    function ativarUsuario($id){
         $dataatual = NULL;
         $sql = "UPDATE tbl_usuario SET
          excluido_em = :atual
          WHERE id_usuario = :id";
         $stmt = $this->db->prepare($sql);
-        $stmt->bindParam(':id', $id_usuario);
+        $stmt->bindParam(':id', $id);
         $stmt->bindParam(':atual', $dataatual);
         if($stmt->execute()){
             return true;
@@ -118,3 +97,4 @@ class Usuario{
         }
     }
 }
+ 
