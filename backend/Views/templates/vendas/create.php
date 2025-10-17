7f8c0b988a8fc88745242a254890d123fc8df2f0
KEZIA SILVA DO ESPIRITO SANTO <kezia.sesanto@senacsp.edu.br>
2025-10-17 10:48:24 -0300

    Add botão adicionar produtos

--- backend/Views/templates/vendas/create.php
+++ backend/Views/templates/vendas/create.php
@@ -21,4 +21,37 @@
 <input type="radio" name="forma_pagamento" id="forma_pagamento" value required>
 <br>
 <button type="submit">Salvar</button>
+<div class="mb-3">
+        <label class="w3-text-blue">Produtos</label>
+        <div id="produtos-container"></div>
+    <button type="button" onclick="addProduto()" class="w3-button w3-teal">Adicionar Produto</button>
+</div>
 </form>
+
+<script>
+let produtoCount = 0;
+function addProduto() {
+        produtoCount++;
+        const produtoDiv = document.createElement('div');
+        produtoDiv.setAttribute('id', `produto-${produtoCount}`);
+        produtoDiv.classList.add('mb-3');
+        produtoDiv.innerHTML = `
+            <p>produto ${produtoCount} <button type="button" onclick="removeproduto(${produtoCount})" class="w3-button w3-red">Remover produto</button></p>
+            <div id="carrinho-container-${produtoCount}">
+                <div class="mb-3">
+                    <label class="w3-text-blue">Produto</label>
+                    <input type="text" name="carrinho[${produtoCount}][]" placeholder="Nome do produto ${produtoCount}" class="w3-input w3-border" required>
+                </div>
+                <div class="mb-3">
+                    <label class="w3-text-blue">Quantidade</label>
+                    <input type="text" name="quantidade" placeholder="Quantidade do produto ${produtoCount}" class="w3-input w3-border" required>
+                </div>
+            </div>
+        `;
+        document.getElementById('produtos-container').appendChild(produtoDiv);
+    }
+    function removeproduto(semestreId) {
+        const semestreDiv = document.getElementById(`produto-${semestreId}`);
+        semestreDiv.remove();
+    }
+</script>
