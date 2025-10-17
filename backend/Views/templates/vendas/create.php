328ee480cb24b17398340e4bb05cf55d95627274
KEZIA SILVA DO ESPIRITO SANTO <kezia.sesanto@senacsp.edu.br>
2025-10-17 10:10:56 -0300

    Model Vendas quase completa

--- backend/Views/templates/vendas/create.php
+++ backend/Views/templates/vendas/create.php
@@ -1,4 +1,4 @@
-<div>Sou o create</div>
+<h2>Cadastrar/Criar um registro de venda:</h2>
 <form action="/backend/vendas/salvar" method="post" enctype="multipart/form-data">
 <label for="Data">Data</label>
 <input type="date" name="data_venda" id="data_venda" required>
@@ -6,7 +6,18 @@
 <label for="Valor">Valor</label>
 <input type="text" name="valor_total" id="valor_total" required>
 <br>
-<label for="Pagamento">Forma de Pagamento</label>
+<h4>Forma de pagamento</h4>
+<br>
+<label for="Dinheiro">Dinheiro</label>
+<input type="radio" name="forma_pagamento" id="forma_pagamento" value required>
+<br>
+<label for="Cartão">Cartão</label>
+<input type="radio" name="forma_pagamento" id="forma_pagamento" value required>
+<br>
+<label for="Transferência">Transferência</label>
+<input type="radio" name="forma_pagamento" id="forma_pagamento" value required>
+<br>
+<label for="Pix">Pix</label>
 <input type="radio" name="forma_pagamento" id="forma_pagamento" value required>
 <br>
 <button type="submit">Salvar</button>
--- backend/Views/templates/vendas/edit.php
+++ backend/Views/templates/vendas/edit.php
@@ -1,14 +1,25 @@
-<div>Sou o edit</div>
-<form action="/backend/vendas/atualizar/ <?php echo $vendas['id_venda']; ?>" method="post" 
-enctype="multipart/form-data">
-<label for="Data">Data</label>
-<input type="date" name="data_venda" id="id_venda" value="<?php echo $vendas['data_venda']; ?>" required>
-<br>
-<label for="Valor">Valor Total</label>
-<input type="text" name="valor_total" id="id_venda" value="<?php echo $vendas['valor_total']; ?>" required>
-<br>
-<label for="Pagamento">Forma de Pagamento</label>
-<input type="text" name="forma_pagamento" id="id_venda" value="<?php echo $vendas['forma_pagamento']; ?>" required>
-<br>
-<button type="submit">Salvar</button>
-</form>
+
+<div class="w3-container w3-card-4 w3-light-grey w3-text-blue w3-margin w3-center" style="max-width:800px;">
+    <h1>Editar registro da venda</h1>
+     <form action="/backend/vendas/atualizar" method="POST" class="w3-panel w3-center">
+        <div class="w3-row w3-section">
+            <div class="w3-col" style="width:50px"><i class="w3-xxlarge fa fa-envelope-o"></i></div>
+            <div class="w3-rest">
+                <input class="w3-input w3-border" name="data_venda" type="text" id="data_venda" value="<?php echo $vendas['data_venda']; ?>" required>
+            </div>
+        </div>
+        <div class="w3-row w3-section">
+            <div class="w3-col" style="width:50px"><i class="w3-xxlarge fa fa-lock"></i></div>
+            <div class="w3-rest">
+                <input class="w3-input w3-border" name="valor_total" type="text" id="valor_total" value="<?php echo $vendas['valor_total']; ?>" required>
+            </div>
+        </div>
+        <div class="w3-row w3-section">
+            <div class="w3-col" style="width:50px"><i class="w3-xxlarge fa fa-lock"></i></div>
+            <div class="w3-rest">
+                <input class="w3-input w3-border" name="forma_pagamento" type="text" id="forma_pagamento" value="<?php echo $vendas['forma_pagamento']; ?>" required>
+            </div>
+        </div>
+        <button type="submit" class="w3-button w3-blue">Salvar</button>
+    </form>
+</div>
