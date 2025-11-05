1d9a9a9178dd1d4309f0dfef4e9e998c493d5187
nath-sans <cristinenathaly5@gmail.com>
2025-11-05 10:00:17 -0300

    ajustes

--- backend/Core/Flash.php
+++ backend/Core/Flash.php
@@ -10,15 +10,9 @@ class Flash
             'type' => $type,
             'message' => $message
         ];
-<<<<<<< HEAD
        
     }
  
-=======
-        
-    }
-
->>>>>>> 30e42a079eaa155a1ba55a4d90bef79ef1323ddb
     public static function get(){
         if(!isset($_SESSION)){
             session_start();
@@ -27,10 +21,7 @@ class Flash
             $flash = $_SESSION['flash'];
             unset($_SESSION['flash']);
             return $flash;
-<<<<<<< HEAD
             
-=======
->>>>>>> 30e42a079eaa155a1ba55a4d90bef79ef1323ddb
         }
         return null;
     }
--- backend/Views/templates/autor/create.php
+++ backend/Views/templates/autor/create.php
@@ -1,25 +1,33 @@
 <div class="w3-container">
-    <h3>Novo Autor</h3>
-    <form action="/backend/autor/salvar" method="POST" enctype="multipart/form-data" class="w3-container w3-card-4">
+    <h3>Gerenciar Autores</h3>
+    <a href="/backend/autor/criar" class="w3-button w3-blue">Adicionar Novo Autor</a>
+    <br><br>
+    
+    <table class="w3-table w3-striped w3-bordered w3-border w3-hoverable w3-white">
+        <thead>
+            <tr class="w3-blue">
+                <th>Nome</th>
+                <th>Biografia</th>
+                <th>Ações</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($autores as $escritor): ?>
+                <tr>
+                    <td><?= htmlspecialchars($escritor['nome_autor']); ?></td>
+                    <td><?= htmlspecialchars(substr($escritor['biografia'], 0, 50)); ?>...</td>
+                    <td>
+                        <a href="/backend/autor/editar/<?= $escritor['id_autor']; ?>" class="w3-button w3-tiny w3-khaki">Editar</a>
+<form action="/backend/autor/excluir/<?= $escritor['id_autor']; ?>" method="POST" style="display:inline;">
+    <input type="hidden" name="id_autor" value="<?= $escritor['id_autor']; ?>">
+    <button type="submit" class="w3-button w3-tiny w3-red" onclick="return confirm('Tem certeza que deseja excluir este autor?')">
+        Excluir
+    </button>
+</form>
 
-        <p>
-        <label class="w3-text-blue"><b>Nome do Autor</b></label>
-        <input class="w3-input w3-border" name="nome_autor" type="text" required>
-        </p>
-
-        <p>
-        <label class="w3-text-blue"><b>Biografia</b></label>
-        <textarea class="w3-input w3-border" name="biografia_autor" rows="4" placeholder="Escreva uma breve biografia..."></textarea>
-        </p>
-
-        <p>
-        <label class="w3-text-blue"><b>Foto do Autor</b></label>
-        <input class="w3-input w3-border" name="foto_autor" type="file" accept="image/*">
-        </p>
-
-        <p>
-        <button class="w3-button w3-blue">Salvar Autor</button>
-        </p>
-
-    </form>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
 </div>
